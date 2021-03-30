60a867c660712d83b6c0caee744c4f3e8670f919
Harshitrich <74772358+Harshitrich@users.noreply.github.com>
2021-03-31 00:31:28 +0530

    Update Header.jsx

--- pkg/ui/v1beta1/frontend/src/components/Menu/Header.jsx
+++ pkg/ui/v1beta1/frontend/src/components/Menu/Header.jsx
@@ -3,23 +3,23 @@ import { connect } from 'react-redux';
 
 import { Link } from 'react-router-dom';
 
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
 
 import Menu from './Menu';
 
 import { toggleMenu } from '../../actions/generalActions';
 
-const useStyles = makeStyles({
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-});
+// const useStyles = makeStyles({
+//   menuButton: {
+//     marginLeft: -12,
+//     marginRight: 20,
+//   },
+// });
 
 const KatibLink = withStyles({
   root: {
@@ -31,24 +31,24 @@ const KatibLink = withStyles({
 })(Typography);
 
 const Header = props => {
-  const classes = useStyles();
+//   const classes = useStyles();
 
-  const toggleMenu = event => {
-    props.toggleMenu(true);
-  };
+//   const toggleMenu = event => {
+//     props.toggleMenu(true);
+//   };
 
   return (
     <div>
       <AppBar position={'static'} color={'primary'}>
         <Toolbar>
-          <IconButton
+    {/* <IconButton
             className={classes.menuButton}
             color={'inherit'}
             aria-label={'Menu'}
             onClick={toggleMenu}
           >
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
           <KatibLink variant={'h5'} color={'secondary'} component={Link} to="/">
             Katib
           </KatibLink>
